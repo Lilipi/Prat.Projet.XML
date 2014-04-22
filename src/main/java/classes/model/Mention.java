ac96f9e547bc6c132cb5dea632e4f40e5eac7266
Lilipi <prat.emi@gmail.com>
2014-04-22 18:14:35 +0200

    Correction annotations Xml enumeration

--- src/main/java/classes/model/Mention.java
+++ src/main/java/classes/model/Mention.java
@@ -1,15 +1,20 @@
 package classes.model;
 
 import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
 
 /**
  * Created by Emilie on 08/04/14.
  */
 @XmlEnum(String.class)
 public enum Mention {
+    @XmlEnumValue("Passable")
     PASSABLE("Passable"),
+    @XmlEnumValue("Assez bien")
     AB("Assez bien"),
+    @XmlEnumValue("Bien")
     BIEN("Bien"),
+    @XmlEnumValue("Très bien")
     TB("Très bien");
 
 
